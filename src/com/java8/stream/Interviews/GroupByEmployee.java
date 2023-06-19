5259e75568bd16c99c7433456210f229de61eb0d
kirankumarkarmala1996 <108362315+kirankumarkarmala1996@users.noreply.github.com>
2023-06-19 15:43:32 +0530

    19thJune
    19thJune

--- src/com/java8/stream/Interviews/GroupByEmployee.java
+++ src/com/java8/stream/Interviews/GroupByEmployee.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.java8.stream.exercise.Student;
 
@@ -21,18 +22,23 @@ public class GroupByEmployee {
 		employees.add(new Employee(8,"gopi k","IT"));
 		employees.add(new Employee(9,"vishnu","EEE"));
 		
-		Map<String, List<Employee>>map = new HashMap<String, List<Employee>>();
-		for(Employee empl :employees) {
-			if(!map.containsKey(empl.getDept())) {
-				List<Employee>emp = new ArrayList<Employee>();
-				emp.add(empl);
-				map.put(empl.getDept(), emp);
-			}else {
-				List<Employee>empList=map.get(empl.getDept());
-				empList.add(empl);
-				map.put(empl.getDept(), empList);				
-		}
-		}
+//Before java8
+		
+//		Map<String, List<Employee>>map = new HashMap<String, List<Employee>>();
+//		for(Employee empl :employees) {
+//			if(!map.containsKey(empl.getDept())) {
+//				List<Employee>emp = new ArrayList<Employee>();
+//				emp.add(empl);
+//				map.put(empl.getDept(), emp);
+//			}else {
+//				List<Employee>empList=map.get(empl.getDept());
+//				empList.add(empl);
+//				map.put(empl.getDept(), empList);				
+//		}
+//		}
+		
+//		AfterJava8
+		Map<String,List<Employee>>map=employees.stream().collect(Collectors.groupingBy(Employee::getDept));
 		map.forEach((k,v)->{
 			System.out.println(k+":"+v);
 		});
